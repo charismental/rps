b84b2f2c4a18ac231a0ecf6100ecab1763a14f8a
Tristin <thewett-c@sbgtv.com>
2020-10-22 12:31:20 -0700

    Add Vuetify, change name in app

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="text-h6 font-weight-medium">
+        RockPaperScissors
+        <!-- <v-icon>fa fa-lock</v-icon> -->
+      </div>
+
+      <v-spacer />
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <hello-world />
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+  import Vue from 'vue'
+  import HelloWorld from './components/HelloWorld.vue'
 
-export default Vue.extend({
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-});
+  export default Vue.extend({
+    name: 'App',
+
+    components: {
+      HelloWorld,
+    },
+
+    data: () => ({
+    //
+    }),
+  })
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,13 @@
+import 'font-awesome/css/font-awesome.min.css'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify)
+
+const vuetify = new Vuetify({
+    icons: {
+        iconfont: 'fa4',
+      },
+})
+
+export default vuetify
