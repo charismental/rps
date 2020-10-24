1928b52889c64d338ad5b305c63a1c3fc96dc606
Tristin <thewett-c@sbgtv.com>
2020-10-23 18:09:33 -0700

    Adjusting styling for mobile

--- src/components/Scoreboard.vue
+++ src/components/Scoreboard.vue
@@ -5,20 +5,27 @@
     max-width="600"
     height="150"
   >
-    <v-row>
+    <v-row class="mt-6">
       <v-col
         cols="3"
-        class="offset-2 font-weight-bold text-h2"
+        class="offset-md-2 offset-2 font-weight-bold"
+        :class="$vuetify.breakpoint.smAndUp ? 'text-h2' : 'text-h4'"
       >
         You
       </v-col>
-      <v-col cols="3">
-        <div class="font-weight-bold text-h2">
+      <v-col
+        cols="3"
+      >
+        <div
+          class="font-weight-bold"
+          :class="$vuetify.breakpoint.smAndUp ? 'text-h2' : 'text-h4'"
+        >
           {{ userScore }} - {{ compScore }}
         </div>
       </v-col>
       <v-col
-        class="font-weight-bold text-h2"
+        class="font-weight-bold"
+        :class="$vuetify.breakpoint.smAndUp ? 'text-h2' : 'text-h4'"
         cols="3"
       >
         Com
